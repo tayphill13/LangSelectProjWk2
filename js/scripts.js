bed1f6a78b9668d99bd688e688a7b035d4c95bee
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-27 14:08:26 -0700

    add 3 additional result values in js

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,19 @@ $(document).ready(function() {
 
   // var result6 
 
-  if (result1 === result2 || result3) {
-    result = "Introverts might like to express themselves with the cool stylings of HTML with CSS!"
+  if (result1 === result2 || result2 === result5) {
+      result = "Try out the cool stylings of HTML with CSS and express yourself!"
   }
-    else if (result2 === result3) {
-      result = "You might consider the Cruel Mistress that is JavaScript, and don't forget to use try out Jquery!"
+    else if (result3 === result4) {
+      result = "You might consider the Cruel Mistress that is JavaScript, and don't forget to try out Jquery!"
   }
+    else if (result4 === result5) {
+      result = "You seem chill, you could probably choose whichever language you like.  Did you know Swift is a compiled programming language developed by Apple?"
+  }
+    else if (result === result) {
+      result = "Why don't you give the general-purpose programming language, Ruby a try!  It was developed by Yukihiro Matsumoto in the mid-90s"
+    }
+      // else if ()
   $("#output").text(result);
   });
 });
