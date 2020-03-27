6b49732a3de843e3b772d5d78ab9cf52da134c38
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-27 11:54:40 -0700

    variable resultOne equal to what jquery receives from user at checkbox questionOne

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
   event.preventDefault();
 
-  var result1
+  var result1 = $("input[name=questionOne]:checked").val();
 
-  var result2
+  var result2 = $("input[name=questionTwo]:checked").val();
 
   var result3
 
   var result4
 
   var result5
+
+  if (result)
 
   });
 });
